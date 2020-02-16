b1787474f08bfd300221408d17595d4d171c3290
Zhuoqian <yzhq97@gmail.com>
2020-02-16 12:02:35 -0500

    Finished: task 5 TODO: task 6, 7, 9

--- DrawSVG/src/viewport.cpp
+++ DrawSVG/src/viewport.cpp
@@ -13,30 +13,24 @@ void ViewportImp::set_viewbox( float centerX, float centerY, float vspan ) {
   this->centerY = centerY;
   this->vspan = vspan;
 
-//  Matrix3x3 B;
-//
-//  B(0,0) = 0.; B(0,1) = 0.; B(0,2) = 0.;
-//  B(1,0) = 0.; B(1,1) = 0.; B(1,2) = 0.;
-//  B(2,0) = 0.; B(2,1) = 0.; B(2,2) = 0.;
-//
-//  set_svg_2_norm(B);
+  float s = 1.0f / (2.0f * vspan);
 
-  Matrix3x3 x = svg_2_norm;
-  printf("\n");
-  printf("%.3f %.3f %.3f\n", centerX, centerY, vspan);
-  printf("%.3f %.3f %.3f\n", x(0, 0), x(0, 1), x(0, 2));
-  printf("%.3f %.3f %.3f\n", x(1, 0), x(1, 1), x(1, 2));
-  printf("%.3f %.3f %.3f\n", x(2, 0), x(2, 1), x(2, 2));
+  Matrix3x3 x;
+  x(0, 0) = s;  x(0, 1) = 0.; x(0, 2) = 0.5 - s * centerX;
+  x(1, 0) = 0.; x(1, 1) = s;  x(1, 2) = 0.5 - s * centerY;
+  x(2, 0) = 0.; x(2, 1) = 0.; x(2, 2) = 1.;
+
+  set_svg_2_norm(x);
+
+}
+
+void ViewportImp::update_viewbox( float dx, float dy, float scale ) {
 
   this->centerX -= dx;
   this->centerY -= dy;
   this->vspan *= scale;
-}
 
-void ViewportImp::update_viewbox( float dx, float dy, float scale ) { 
-  
-
-  set_viewbox( dx, dy, scale );
+  set_viewbox( centerX, centerY, vspan );
 }
 
 } // namespace CMU462
